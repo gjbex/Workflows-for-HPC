a41829554d0f3433ed09cc8c6765132dd63f8c34
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-01-21 11:05:03 +0100

    Fix type

--- source-code/job-dependencies/dangling-jobs/submit_workflow.sh
+++ source-code/job-dependencies/dangling-jobs/submit_workflow.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 JOB1_ID=$(sbatch job1.slurm | grep -oP '\b\d+\b')
 
 # job to run after job1 ends successfully
 sbatch --dependency=afterok:$JOB1_ID job2.slurm
 
 # job to run after job1 fails
-sbacth --dependency=afternotok:$JOB1_ID job3.slurm
+sbatch --dependency=afternotok:$JOB1_ID job3.slurm
