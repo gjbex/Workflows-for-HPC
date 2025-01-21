b98e5e697f1b0b22b332bdc563b0061ecab12b66
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-01-21 14:13:56 +0100

    Change code to ues only Slurm variables

--- source-code/job-arrays/submit_array_job.sh
+++ source-code/job-arrays/submit_array_job.sh
@@ -5,9 +5,7 @@ set -e
 # compute batch size from the number of batches
 NR_BATCHES=4
 NR_FILES=$(ls input/data_*.dat | wc -l)
-export BATCH_SIZE=$(( $NR_FILES / $NR_BATCHES ))
+BATCH_SIZE=$(( $NR_FILES / $NR_BATCHES ))
 
 # submit an array job with the number of batches
-sbatch --array=0-$(( $NR_BATCHES - 1 )) \
-       --export=ALL,BATCH_SIZE=$BATCH_SIZE \
-       process_batch.slurm
+sbatch --array=1-$NR_FILES:$BATCH_SIZE process_batch.slurm
