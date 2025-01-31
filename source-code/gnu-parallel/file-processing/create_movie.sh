25cfe1e20d062657fd8adaf60f77ba3ef9ac173d
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-01-31 09:54:10 +0100

    Add all steps of the workflow to the script

--- source-code/gnu-parallel/file-processing/create_movie.sh
+++ source-code/gnu-parallel/file-processing/create_movie.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
 
-parallel --max-procs 4 --arg-file beta.txt \
-    ./create_plot.py --output_dir images --beta {}
+MAX_BETA=50
+BETA_FILE=beta.txt
+IMG_DIR=images/
+DELAY=20
+LOOP=0
+
+# create file containing the values of beta
+rm -f $BETA_FILE
+for i in $(seq -w 0 $MAX_BETA)
+do
+    echo ${i:0:1}.${i:1} >> $BETA_FILE
+done
+
+# generate a plot of each value of beta
+mkdir -p $IMG_DIR
+parallel --max-procs 4 --arg-file $BETA_FILE \
+    ./create_plot.py --output_dir $IMG_DIR --beta {}
+
+# convert each file from PNG to GIF
+ls $IMG_DIR/*.png | parallel --max-procs 4 \
+    convert {} {.}.gif
+ 
+# convert GIF files to animated GIF
+convert -delay $DELAY -loop $LOOP $IMG_DIR/*.gif movie.gif
