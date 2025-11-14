b4b682bfc68a7123efa4d5302a1c5ba0e9e6c0bb
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-11-14 13:44:29 +0100

    Simplify and modernize script

--- source-code/atools/reductor/pickle_diff.py
+++ source-code/atools/reductor/pickle_diff.py
@@ -15,17 +15,10 @@ if __name__ == '__main__':
         right_dict = pickle.load(right_file)
     left_words = set(left_dict.keys())
     right_words = set(right_dict.keys())
-    left_only = left_words - right_words
-    for word in left_only:
-        print('< {word:s}: {count:10d}'.format(word=word,
-                                               count=left_dict[word]))
-    common = left_words.intersection(right_words)
-    fmt_str = '{word:s}: <{l_count:10d} >{r_count:10d}'
-    for word in common:
+    for word in left_words - right_words:
+        print(f'< {word}: {left_dict[word]}')
+    for word in left_words & right_words:
         if left_dict[word] != right_dict[word]:
-            print(fmt_str.format(word=word, l_count=left_dict[word],
-                                 r_count=right_dict[word]))
-    right_only = right_words - left_words
-    for word in right_only:
-        print('< {word:s}: {count:10d}'.format(word=word,
-                                               count=right_dict[word]))
+            print(f'{word}: <{left_dict[word]} >{right_dict[word]}')
+    for word in right_words - left_words:
+        print(f'< {word}: {right_dict[word]}')
