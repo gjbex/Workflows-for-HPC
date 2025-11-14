60bf08799e681f87d82809d08918197999efd6ac
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-11-14 13:45:05 +0100

    Make script tolerate and ignore a second argument

--- source-code/atools/reductor/pickle_empty.py
+++ source-code/atools/reductor/pickle_empty.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from argparse import ArgumentParser
 import pickle
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     arg_parser = ArgumentParser(description='create pickle file of empty '
                                             'dictionary')
     arg_parser.add_argument('out', help='name of the pickle file')
-    options = arg_parser.parse_args()
-    counter = dict()
+    options, _ = arg_parser.parse_known_args()
+    counter = {}
     with open(options.out, 'wb') as out_file:
         pickle.dump(counter, out_file)
